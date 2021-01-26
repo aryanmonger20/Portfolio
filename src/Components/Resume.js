570a433b6f3b746509bf4b150389341df0df2bd7
Aryan Gupta <aryangupta007yo@gmail.com>
2021-01-27 02:37:03 +0530

    main

---
+++ src/Components/Resume.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+class Resume extends Component {
+  render() {
+
+    if(this.props.data){
+      var skillmessage = this.props.data.skillmessage;
+      
+     
+      var skills = this.props.data.skills.map(function(skills){
+        var className = 'bar-expand '+skills.name.toLowerCase();
+        return <li key={skills.name}><span style={{width:skills.level}}className={className}></span><em>{skills.name}</em></li>
+      })
+    }
+
+    return (
+      <section id="resume">
+
+      <div className="row skill">
+
+         <div className="three columns header-col">
+            <h3><h1><span>Skills</span></h1></h3>
+         </div>
+
+         <div className="nine columns main-col">
+
+            <h2>{skillmessage}
+            </h2>
+            <br/>
+            <br></br>
+          
+
+				<div className="bars">
+				   <ul className="skills">
+					  {skills}
+					</ul>
+				</div>
+			</div>
+      </div>
+   </section>
+    );
+  }
+}
+
+export default Resume;
